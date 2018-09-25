0c7723b94b51ef9c131878abd7f5b18c945b94fb
Vlad-Iliescu <vlad.iliescu.88@gmail.com>
2018-09-25 19:42:51 +0300

    Ratchet basic example

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$entryData = array(
+    'category' => "kittensCategory",
+    'title' => "my_title",
+    'article' => "my_article",
+    'when' => time()
+);
+
+
+// This is our new stuff
+$context = new ZMQContext();
+$socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'my pusher');
+$socket->connect("tcp://localhost:5555");
+
+$socket->send(json_encode($entryData));
---
+++ src/Pusher.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace LaravelThruway;
+
+
+use Ratchet\ConnectionInterface;
+use Ratchet\Wamp\Topic;
+use Ratchet\Wamp\WampServerInterface;
+
+class Pusher implements WampServerInterface
+{
+
+    protected $subscribedTopics = [];
+
+    /**
+     * When a new connection is opened it will be passed to this method
+     * @param  ConnectionInterface $conn The socket/connection that just connected to your application
+     * @throws \Exception
+     */
+    public function onOpen(ConnectionInterface $conn)
+    {
+        var_dump("onOpen");
+    }
+
+    /**
+     * This is called before or after a socket is closed (depends on how it's closed).  SendMessage to $conn will not result in an error if it has already been closed.
+     * @param  ConnectionInterface $conn The socket/connection that is closing/closed
+     * @throws \Exception
+     */
+    public function onClose(ConnectionInterface $conn)
+    {
+        var_dump("onCLose");
+    }
+
+    /**
+     * If there is an error with one of the sockets, or somewhere in the application where an Exception is thrown,
+     * the Exception is sent back down the stack, handled by the Server and bubbled back up the application through this method
+     * @param  ConnectionInterface $conn
+     * @param  \Exception $e
+     * @throws \Exception
+     */
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        var_dump("onError: " . $e->getMessage());
+    }
+
+    /**
+     * An RPC call has been received
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string $id The unique ID of the RPC, required to respond to
+     * @param string|Topic $topic The topic to execute the call against
+     * @param array $params Call parameters received from the client
+     */
+    public function onCall(ConnectionInterface $conn, $id, $topic, array $params)
+    {
+        var_dump("onCall");
+    }
+
+    /**
+     * A request to subscribe to a topic has been made
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string|Topic $topic The topic to subscribe to
+     */
+    public function onSubscribe(ConnectionInterface $conn, $topic)
+    {
+        var_dump("onSubscribe: " . $topic->getId());
+        $this->subscribedTopics[$topic->getId()] = $topic;
+    }
+
+    /**
+     * A request to unsubscribe from a topic has been made
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string|Topic $topic The topic to unsubscribe from
+     */
+    public function onUnSubscribe(ConnectionInterface $conn, $topic)
+    {
+        var_dump("onUnSubscribe: " . $topic->getId());
+    }
+
+    /**
+     * A client is attempting to publish content to a subscribed connections on a URI
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string|Topic $topic The topic the user has attempted to publish to
+     * @param string $event Payload of the publish
+     * @param array $exclude A list of session IDs the message should be excluded from (blacklist)
+     * @param array $eligible A list of session Ids the message should be send to (whitelist)
+     */
+    public function onPublish(ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible)
+    {
+        var_dump("onPublish");
+    }
+
+    public function onEntry($entry)
+    {
+        var_dump("onEntry: " . $entry);
+
+        $entryData = json_decode($entry, true);
+
+        // If the lookup topic object isn't set there is no one to publish to
+        if (!array_key_exists($entryData['category'], $this->subscribedTopics)) {
+            return;
+        }
+
+        $topic = $this->subscribedTopics[$entryData['category']];
+
+        // re-send the data to all the clients subscribed to that category
+        $topic->broadcast($entryData);
+    }
+}
