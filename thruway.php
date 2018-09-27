d10ae7bfef5800bd8ca1b1c2cc6a49078a5254b8
Vlad-Iliescu <vlad.iliescu.88@gmail.com>
2018-09-27 19:51:49 +0300

    trial 2

--- thruway.php
+++ thruway.php
@@ -1,53 +1,28 @@
 <?php
 
 use LaravelThruway\RatchetTransportProvider;
-use Thruway\ClientSession;
 use Thruway\Peer\Client;
 
 require 'vendor/autoload.php';
 
-$client = new Client("realm1");
-try {
-    $client->addTransportProvider(new RatchetTransportProvider("ws://127.0.0.1:9090/"));
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
+$pusher = new Client("realm1");
 
-$client->on('open', function (ClientSession $session) {
+$loop = React\EventLoop\Factory::create();
 
-    // 1) subscribe to a topic
-    $onevent = function ($args) {
-        echo "Event {$args[0]}\n";
-    };
-    $session->subscribe('com.myapp.hello', $onevent);
+$pusher->on('open', function ($session) use ($loop) {
+    $context = new React\ZMQ\Context($loop);
+    $pull = $context->getSocket(ZMQ::SOCKET_PULL);
+    $pull->bind('tcp://127.0.0.1:5555');
 
-    // 2) publish an event
-    $session->publish('com.myapp.hello', ['Hello, world from PHP!!!'], [], ["acknowledge" => true])->then(
-        function () {
-            echo "Publish Acknowledged!\n";
-        },
-        function ($error) {
-            // publish failed
-            echo "Publish Error {$error}\n";
+    $pull->on('message', function ($entry) use ($session) {
+        $entryData = json_decode($entry, true);
+        if (isset($entryData['category'])) {
+            $session->publish($entryData['category'], [$entryData]);
         }
-    );
-
-    // 3) register a procedure for remoting
-    $add2 = function ($args) {
-        return $args[0] + $args[1];
-    };
-    $session->register('com.myapp.add2', $add2);
-
-    // 4) call a remote procedure
-    $session->call('com.myapp.add2', [2, 3])->then(
-        function ($res) {
-            echo "Result: {$res}\n";
-        },
-        function ($error) {
-            echo "Call Error: {$error}\n";
-        }
-    );
+    });
 });
 
-
-$client->start();
+$router = new Thruway\Peer\Router($loop);
+$router->addInternalClient($pusher);
+$router->addTransportProvider(new Thruway\Transport\RatchetTransportProvider("0.0.0.0", 7474));
+$router->start();
